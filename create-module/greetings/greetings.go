a9e3e99630fe5127aa566986c774244b8f33e2b7
huantingwei <ting02940@gmail.com>
2020-09-17 17:27:39 +0800

    feat: random greeting

--- create-module/greetings/greetings.go
+++ create-module/greetings/greetings.go
@@ -3,12 +3,29 @@ package greetings
 import (
 	"fmt"
 	"errors"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hello! Welcome %v!", name)
+	// message := fmt.Sprintf("Hello! Welcome %v!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats :[]string{
+		"Hi, %v. Welcome!", 
+		"Great to see you %v!",
+		"哈囉 %v!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
